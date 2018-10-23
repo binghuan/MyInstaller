4ea959892c565aef8ffa08684c1ffd7965426ca4
binghuan <binghuanlin@gmail.com>
2018-10-23 13:41:13 +0800

    first check-in

---
+++ app/src/main/java/com/bh/android/myinstaller/MainActivity.java
@@ -0,0 +1,176 @@
+package com.bh.android.myinstaller;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "BH_" + this.getClass().getSimpleName();
+
+    private Button mButton2InstallApp = null;
+    private TextView mTextViewInstallable = null;
+
+    private PackageManager mPkgMgr = null;
+
+    private Context mContext = null;
+
+    private final int MY_PERMISSIONS_REQUEST_INSTALL_PACKAGES = 1001;
+    private final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1002;
+    private final int REQEUST_CODE = 1234;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQEUST_CODE && resultCode == Activity.RESULT_OK) {
+            if (getPackageManager().canRequestPackageInstalls()) {
+                callInstallProcess();
+            }
+        } else {
+            //give the error
+        }
+
+        updateIsAbleToInstallApk();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                    tryToInstallApk();
+
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
+    private void callInstallProcess() {
+        try {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            String targetPkgName = "com.syezon.wifikey.apk";
+            File apkFile = new File(
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
+                    targetPkgName);
+            Log.v(TAG, "Path of Apk file: " + apkFile.getAbsoluteFile());
+            Uri apkUri = FileProvider.getUriForFile(mContext, BuildConfig.APPLICATION_ID + ".fileprovider", apkFile);
+
+            Log.v(TAG, "apkUri: " + apkUri.getPath());
+
+            Intent installIntent = new Intent(Intent.ACTION_VIEW);
+            installIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            installIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            installIntent.setDataAndType(apkUri, "application/vnd.android.package-archive");
+            startActivity(installIntent);
+        } catch (Exception e) {
+            //LogUtilities.show(this, e);
+            Log.e(TAG, e.toString());
+        }
+    }
+
+    private void tryToInstallApk() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (!getPackageManager().canRequestPackageInstalls()) {
+                startActivityForResult(
+                        new Intent(Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES)
+                                .setData(Uri.parse(String.format("package:%s", getPackageName()))), REQEUST_CODE);
+            } else {
+                callInstallProcess();
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mContext = this;
+        mPkgMgr = mContext.getPackageManager();
+
+        mButton2InstallApp = (Button) findViewById(R.id.button_to_request_install);
+        mButton2InstallApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Here, thisActivity is the current activity
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+                    // Here, thisActivity is the current activity
+                    if (ContextCompat.checkSelfPermission(mContext,
+                            Manifest.permission.READ_EXTERNAL_STORAGE)
+                            != PackageManager.PERMISSION_GRANTED) {
+
+                        // Permission is not granted
+                        // Should we show an explanation?
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                                Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                            // Show an explanation to the user *asynchronously* -- don't block
+                            // this thread waiting for the user's response! After the user
+                            // sees the explanation, try again to request the permission.
+                        } else {
+                            // No explanation needed; request the permission
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+
+                            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                            // app-defined int constant. The callback method gets the
+                            // result of the request.
+                        }
+                    } else {
+                        // Permission has already been granted
+                        tryToInstallApk();
+                    }
+
+                } else {
+                    callInstallProcess();
+                }
+
+            }
+        });
+        mTextViewInstallable = (TextView) findViewById(R.id.textViewInstallable);
+        updateIsAbleToInstallApk();
+    }
+
+    private void updateIsAbleToInstallApk() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            boolean isInstallable = mPkgMgr.canRequestPackageInstalls();
+            mTextViewInstallable.setText("canRequestPackageInstalls: " + isInstallable);
+        }
+    }
+}
